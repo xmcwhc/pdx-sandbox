875ccd104ed51d9c9648ff6db1124ddecf7598ad
jz <jz@pdx.ltd>
2018-11-05 01:19:12 +0800

    update image names and comments

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,4 +1,13 @@
+# dapp-java
+#
+# 	PDX blockchain hypercloud sandbox for java-based smart-contracts 	
+#
+# VERSION 0.1
+
 FROM openjdk:8-jre-alpine
+
+LABEL maintainer="jz@pdx.ltd"
+
 RUN apk add --no-cache bash
 
 COPY run-java.sh /run.sh

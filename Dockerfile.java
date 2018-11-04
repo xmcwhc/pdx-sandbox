7075bde2a7eddb90e1679ae0dc1979134e73e135
jz <jz@pdx.ltd>
2018-11-04 13:03:44 +0800

    supporting java and native chaincode sandboxing

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,7 +1,7 @@
 FROM openjdk:8-jre-alpine
 RUN apk add --no-cache bash
 
-COPY run.sh /run.sh
+COPY run-java.sh /run.sh
 
 ENTRYPOINT ["/run.sh"]
 
---
+++ run-exec.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DAPP_HOME=/dapps
+
+if [ -z "$1" ]
+	then	
+		echo "No exec supplied, exiting"	
+		exit -1
+
+fi
+
+echo "Starting chain code: " $1
+
+$DAPP_HOME/exec/$1
+
+echo "Shutdown chain code: " $1
+
+exit 0
+
